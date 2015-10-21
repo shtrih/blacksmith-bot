198d509222e9d4dea4abcb70018a8d7339a7baf6
Dmitry M <s@hitagi.ru>
2015-10-21 16:33:49 +0400

    Fix: если путь содержал пробелы, то запуск был невозможен

--- launcher.py
+++ launcher.py
@@ -8,7 +8,7 @@ import os, sys, time
 
 interpreter = sys.executable
 kernel = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BlackSmith.py')
-launch_target = interpreter + " " + kernel
+launch_target = interpreter + ' "' + kernel + '"'
 
 del sys
 while True:
